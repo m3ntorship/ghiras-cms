e536186d048ecad8c9f2c155ec80b8984f8199f4
user <great.nile@gmail.com>
2020-04-24 01:43:44 +0200

    tls verification

---
+++ api/tls/controllers/tls.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * A set of functions called "actions" for `tls`
+ */
+
+module.exports = {
+  challenge: async (ctx, next) => {
+    ctx.send('pgh9jLpXs_JXWSRAWYl-wVVAv87urElk38PagLka28s.mE9W93nQJrdvJ2CORIi9qQ4oF8bJMV1iFcx6UHUBAKk');
+  }
+};
