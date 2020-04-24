711c29baef0fc16a8e1b0a223f9c0f1498028ce9
user <great.nile@gmail.com>
2020-04-24 02:29:45 +0200

    tls verification5

--- api/charity/controllers/charity.js
+++ api/charity/controllers/charity.js
@@ -6,6 +6,6 @@
 
 module.exports = {
   challenge: async (ctx, next) => {
-    ctx.send('ik3LKWZrrbtNyptIurDNJ6DtU1IvhOrqJcAGotIlejM.FxSGnMTPYpwEbXXzAAEbQI-QNicUbiqFOfIiRpPoeSM');
+    ctx.send('bIb_i-DSNRF-vT2LuFYvLw5rMda7YscxAKlFYksfcU4.Ru3XLKLICsCt3fb7240M854XzF4ey6-l2756pD9q6_I');
   }
 };
